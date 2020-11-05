fa0d4bf6325ab03241e88c0ea721845140e04637
AliLop”git config --global user.email alicia.hl87@gmail.comgit config --global user.name AliLop” <65405578+AliLop@users.noreply.github.com>
2020-11-05 11:56:28 +0000

    done

--- index.js
+++ index.js
@@ -17,11 +17,55 @@ mongoose
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
+    //return self.connection.dropDatabase();
   })
   .then(() => {
+    console.log(`connection made`)
     // Run your code here, after you have insured that the connection was made
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+/* CREATE
+  Recipe.create({
+    title: 'Paella',
+    cuisine: 'Spanish'
+  }).then((resp) => {
+      console.log(`${resp[0].title}`)
+  }).catch((err) => {
+      console.log(`Error occurred ${err}`)
+  })
+
+
+//MANUALLY IMPORTED ON MONGOOSE SINCE TERMINAL DOES NTO WORK FOR ME
+//mongoimport --db restaurants --collection restaurants --drop --file primer-dataset.json
+
+Recipe.insertMany(data)
+*/
+
+const promise1 = Recipe.find({}, 'title')
+  .then((titles) => {
+    console.log(titles)
+    //mongoose.connection.close();
+  })
+  
+
+const promise2 =  Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese'}, {duration: 100})
+  .then((updatedDuration) => {
+        console.log (`Updated duration on ${updatedDuration}!`)
+        //mongoose.connection.close();
+        }) 
+
+const promise3 = Recipe.deleteOne({ title: 'Carrot Cake'})
+    .then(() => {
+        console.log(`Deleted!`)  
+        //mongoose.connection.close();
+    })
+
+Promise.all([promise1, promise2, promise3]).then(() => {
+    console.log(`all promises are done`)
+    mongoose.connection.close();
+}).catch((err) => {
+    console.log(`Error ${err}`)
+})
